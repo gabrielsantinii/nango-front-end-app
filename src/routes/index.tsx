21184ebb39dc09d780560372ca7fb888088daaa5
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-19 15:31:52 -0300

    redeploy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -24,7 +24,7 @@ export function Routes(): JSX.Element {
                             <RedirectIfAuthed>
                                 <LoginPageLayout />
                             </RedirectIfAuthed>
-                        }
+                        } 
                     />
                     <Route
                         path="/sign-up"
