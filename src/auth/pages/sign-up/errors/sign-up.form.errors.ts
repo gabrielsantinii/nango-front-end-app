2045ce9464381d2ce9885cc882714a48fada291a
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-19 13:33:20 -0300

    feat: sign-up errors mapped

--- src/auth/pages/sign-up/errors/sign-up.form.errors.ts
+++ src/auth/pages/sign-up/errors/sign-up.form.errors.ts
@@ -2,7 +2,7 @@ export const signupFormErrors = {
     name: { required: "O nome da instituição é obrigatório." },
     categories: { required: "Selecione pelo menos uma categoria." },
     contactPerson: {
-        email: { required: "O email do administrador é obrigatório." },
+        email: { required: "O email do administrador é obrigatório.", invalid: "Insira um e-mail válido." },
         phone: { required: "O phone do administrador é obrigatório." },
         name: { required: "O nome do administrador é obrigatório." },
     },
--- src/auth/pages/sign-up/schemas/sign-up.schema.ts
+++ src/auth/pages/sign-up/schemas/sign-up.schema.ts
@@ -3,23 +3,31 @@ import { signupFormErrors } from "../errors";
 import { SignupFormValues } from "../interfaces";
 
 export const SignupSchema: SchemaOf<SignupFormValues> = object().shape({
-    name: string().required(),
+    name: string().required(signupFormErrors.name.required),
     categories: array(),
     website: string(),
     contactPerson: object().shape({
-        name: string().required(),
-        email: string().required().email(),
-        phone: string().required(),
+        name: string().required(signupFormErrors.contactPerson.name.required),
+        email: string()
+            .required(signupFormErrors.contactPerson.email.required)
+            .email(signupFormErrors.contactPerson.email.invalid),
+        phone: string().required(signupFormErrors.contactPerson.phone.required),
     }),
     address: object().shape({
-        state: string().required(),
-        city: string().required(),
+        state: string().required(signupFormErrors.address.state.required),
+        city: string().required(signupFormErrors.address.city.required),
         street: string(),
         neighborhood: string(),
         country: string().required(),
         postalCode: string(),
     }),
     email: string().required().email(),
-    rangeOfEmployees: object().shape({ label: string().required(), value: string().required() }),
-    rangeOfStudents: object().shape({ label: string().required(), value: string().required() }),
+    rangeOfEmployees: object().shape({
+        label: string().required(signupFormErrors.rangeOfEmployees.required),
+        value: string().required(signupFormErrors.rangeOfEmployees.required),
+    }),
+    rangeOfStudents: object().shape({
+        label: string().required(signupFormErrors.rangeOfStudents.required),
+        value: string().required(signupFormErrors.rangeOfStudents.required),
+    }),
 });
